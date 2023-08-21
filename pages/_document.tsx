0e8644f99b28b0c2403ff4a53e74e79012944930
Eric Ramsey <eric.m.ramsey@outlook.com>
2023-08-21 13:36:00 -0400

    changes to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Montserrat:wght@500&display=swap" rel="stylesheet"/>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Montserrat:wght@500&display=swap" rel="style" />
       </Head>
       <body>
         <Main />
