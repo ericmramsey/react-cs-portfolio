dd8f68e1b03093f20e75e84c66a17baa42b91b33
Eric Ramsey <eric.m.ramsey@outlook.com>
2024-06-23 17:22:06 -0400

    commit theme color changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,10 +29,12 @@ module.exports = {
         titleFont: ["Inter", "sans-serif", "#ffffff"],
       },
       boxShadow:{
-        navbarShadow: "0 10px 30px -10px rgba(2,12,27,0.7)",
+        navbarShadow: "0 10px 30px -10px rgba(0,0,0,0.7)"
+        //navbarShadow: "0 10px 30px -10px rgba(2,12,27,0.7)",
       },
       colors: {
-        bodyColor: "#2E3440",
+        bodyColor: "#121212",
+        //bodyColor: "#2E3440",
         textTeal: "#8FBCBB",
         textLight: "#ECEFF4",
         textDark: "#D8DEE9",
