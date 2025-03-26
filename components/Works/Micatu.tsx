ed840a28ad6b6b9d18d016bfbab6e69cc872b3f1
Eric Ramsey <eric.m.ramsey@outlook.com>
2025-03-25 20:10:50 -0400

    finalized version 1.1.0

--- components/Works/Micatu.tsx
+++ components/Works/Micatu.tsx
@@ -24,7 +24,7 @@ const Micatu = () => {
                 <span className="text-textTeal mt-1">
                     <AiFillThunderbolt />
                 </span>
-                •	Developed and maintained production software applications, while also designing and implementing new software solutions using Python.
+                Developed and maintained production software applications, while also designing and implementing new software solutions using Python.
             </li>
             <li className="text-base flex gap-2 text-textDark">
                 <span className="text-textTeal mt-1">
