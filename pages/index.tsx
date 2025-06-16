7323013b89c10cc0d7ed171856d90b84ab7f948c
Eric Ramsey <eric.m.ramsey@outlook.com>
2025-06-16 12:13:34 -0400

    VERSION = '1.2.0.0' VERSION_DESCRIPTION = '''Branch: main(06/16/2025)         • finalized version 1.2.0'''

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,6 @@ export default function Home() {
             <Experience />
             <Projects />
             {/*<Archive />*/}
-            <Archive />
             <Contact />
             <Footer />
           </div>
